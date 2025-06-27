e04e39c4a4e16114e85b4b14d98ba53ae18ae5bd
Lawrence-Wambugu <wambugulawrence@gmail.com>
2025-06-27 11:35:00 +0300

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -31,7 +31,7 @@ function Login() {
             setServerMsg(null);
             setIsSuccess(false);
             try {
-              const res = await fetch('http://localhost:5000/login', {
+              const res = await fetch('/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(values),
