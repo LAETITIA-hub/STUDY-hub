855e9b736b24c9977cdf0c7da9caada0d3d83bab
Lawrence-Wambugu <wambugulawrence@gmail.com>
2025-06-27 07:39:37 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# Initialize the SQLAlchemy instance (to be initialized in app.py)
+# Initialize  SQLAlchemy instance (to be initialized in app.py)
 db = SQLAlchemy()
 
 class User(db.Model):
